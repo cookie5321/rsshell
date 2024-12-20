f5a675f0b7226a007d035413be01334ea82fc2f1
cookie5321 <25889269+cookie5321@users.noreply.github.com>
2024-12-20 16:41:16 +0900

    cleanup code

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,31 @@ use std::os::fd::IntoRawFd;
 use std::os::unix::io::RawFd;
 use std::process::exit;
 
-fn execute_command(mut commands: Vec<String>) -> nix::Result<()> {
-    let mut prev_pipe: Option<(RawFd, RawFd)> = None;
-    let mut output_fd: Option<RawFd> = None;
-    let mut input_fd: Option<RawFd> = None;
+fn run_commands(mut commands: Vec<String>) -> nix::Result<()> {
+    let mut pipe_previous: Option<(RawFd, RawFd)> = None;
+    let mut fd_output: Option<RawFd> = None;
+    let mut fd_input: Option<RawFd> = None;
     let mut children = Vec::new();
+    let command_count = commands.len();
 
-    if commands.len() == 1 {
+    if command_count == 1 {
         let output_position = commands[0].find(">");
         let input_position = commands[0].find("<");
 
         if let (Some(output_position), Some(input_position)) = (output_position, input_position) {
             if output_position > input_position {
-                input_fd = Some(File::open(&commands[0][input_position + 1..output_position].trim())
+                fd_input = Some(File::open(&commands[0][input_position + 1..output_position].trim())
                     .expect("file open failed")
                     .into_raw_fd());
-                output_fd = Some(File::create(&commands[0][output_position + 1..].trim())
+                fd_output = Some(File::create(&commands[0][output_position + 1..].trim())
                     .expect("file creation failed")
                     .into_raw_fd());
                 commands[0] = commands[0][..input_position].trim().to_string();
             } else {
-                input_fd = Some(File::open(&commands[0][input_position + 1..].trim())
+                fd_input = Some(File::open(&commands[0][input_position + 1..].trim())
                     .expect("file open failed")
                     .into_raw_fd());
-                output_fd = Some(File::create(&commands[0][output_position + 1..input_position].trim())
+                fd_output = Some(File::create(&commands[0][output_position + 1..input_position].trim())
                     .expect("file creation failed")
                     .into_raw_fd());
                 commands[0] = commands[0][..input_position].trim().to_string();
@@ -40,62 +41,58 @@ fn execute_command(mut commands: Vec<String>) -> nix::Result<()> {
     }
 
     if let Some((command, file)) = commands[0].split_once(&String::from("<")) {
-        input_fd = Some(File::open(file.trim()).expect("file open failed").into_raw_fd());
+        fd_input = Some(File::open(file.trim()).expect("file open failed").into_raw_fd());
         commands[0] = command.trim().to_string();
     }
-    if let Some((command, file)) = commands.last().unwrap().split_once(&String::from(">")) {
-        output_fd = Some(File::create(file.trim()).expect("file creation failed").into_raw_fd());
-        let len = commands.len();
-        commands[len - 1] = command.trim().to_string();
+    if let Some((command, file)) = commands[command_count - 1].split_once(&String::from(">")) {
+        fd_output = Some(File::create(file.trim()).expect("file creation failed").into_raw_fd());
+        commands[command_count - 1] = command.trim().to_string();
     }
 
-    for (i, command) in commands.iter().enumerate() {
-        let next_pipe = if i < commands.len() - 1 {
+    for (i, command) in commands.into_iter().enumerate() {
+        let pipe_next = if i < command_count - 1 {
             Some(pipe().map(|(x, y)| (x.into_raw_fd(), y.into_raw_fd()))?)
         } else { None };
 
         match unsafe { fork() } {
-            Ok(ForkResult::Child) => {
-                if i == 0 {
-                    if let Some(file_fd) = input_fd {
-                        dup2(file_fd, 0)?;
-                        close(file_fd)?;
-                    }
-                }
-                if i == commands.len() - 1 {
-                    if let Some(output_fd) = output_fd {
-                        dup2(output_fd, 1)?;
-                        close(output_fd)?;
-                    }
-                }
-
-                if let Some((read_fd, _)) = prev_pipe { dup2(read_fd, 0)?; }
-                if let Some((_, write_fd)) = next_pipe { dup2(write_fd, 1)?; }
-
-                let args = process_args(command);
-                let Err(error) = execvp(&args[0], &args);
-
-                println!("Execution failure: {}", error);
-                exit(1);
-            }
             Ok(ForkResult::Parent { child }) => {
                 children.push(waitpid(child, None)?);
 
-                if let Some((_, write_fd)) = next_pipe { close(write_fd)?; }
-                if let Some((read_fd, _)) = prev_pipe { close(read_fd)?; }
+                if let Some((_, fd_write)) = pipe_next { close(fd_write)?; }
+                if let Some((fd_read, _)) = pipe_previous { close(fd_read)?; }
+                if fd_input.is_some() { fd_input = None; }
 
-                prev_pipe = next_pipe;
+                pipe_previous = pipe_next;
             }
-            Err(err) => {
-                println!("Failed to fork: {}", err);
+            Ok(ForkResult::Child) => {
+                if let Some((read_fd, _)) = pipe_previous { dup2(read_fd, 0)?; }
+                if let Some((_, write_fd)) = pipe_next { dup2(write_fd, 1)?; }
+
+                if let Some(file_fd) = fd_input {
+                    dup2(file_fd, 0)?;
+                    close(file_fd)?;
+                }
+                if i == command_count - 1 && fd_output.is_some() {
+                    dup2(fd_output.unwrap(), 1)?;
+                    close(fd_output.unwrap())?;
+                }
+
+                let args = process_args(&command);
+                let Err(error) = execvp(&args[0], &args);
+
+                println!("execution failure: {}", error);
+                exit(1);
+            }
+            Err(error) => {
+                println!("failed to fork: {}", error);
             }
         }
     }
 
-    if let Some(fd) = output_fd {
+    if let Some(fd) = fd_output {
         close(fd)?;
     }
-    if let Some((read_fd, write_fd)) = prev_pipe {
+    if let Some((read_fd, write_fd)) = pipe_previous {
         close(read_fd)?;
         close(write_fd)?;
     }
@@ -113,11 +110,10 @@ fn execute_command(mut commands: Vec<String>) -> nix::Result<()> {
     Ok(())
 }
 
-
 fn process_args(input: &str) -> Vec<CString> {
+    let mut quote = false;
     let mut args = vec![];
     let mut current = String::new();
-    let mut quote = false;
 
     for c in input.chars() {
         if c == '"' { quote = !quote; }
@@ -128,7 +124,6 @@ fn process_args(input: &str) -> Vec<CString> {
     }
 
     if !current.is_empty() { args.push(CString::new(current).unwrap()); }
-
     args
 }
 
@@ -142,17 +137,17 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let input = input.trim();
+        input = input.trim().to_string();
 
-        if input.is_empty() { continue; }
         if input == "exit" { break; }
+        if input.is_empty() { continue; }
 
         let commands: Vec<String> = input
             .split("|")
             .map(|x| x.trim().to_string())
             .collect();
 
-        if let Err(_) = execute_command(commands) { continue; }
+        if let Err(_) = run_commands(commands) { continue; }
     }
 
     println!("Exit oh-my-shell. Bye!");
